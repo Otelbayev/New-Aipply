da72329b9b867ecc54500c8679096b38d2f341d0
Jasurbek <jasurdev104@gmail.com>
2024-10-21 22:23:34 +0500

    new

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,12 +2,13 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Uni from "../components/uni";
 import { page, routes } from "../utils/router";
+import Loading from "../pages/Lid/loading";
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* Suspense wrapping Routes */}
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           {/* Grouped Routes with a shared component (e.g., Uni layout) */}
           <Route element={<Uni />}>
---
+++ src/pages/Lid/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../../assets/icons/logo1.png";
+const Loading = () => {
+  return (
+    <div className="loading">
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+export default Loading;
